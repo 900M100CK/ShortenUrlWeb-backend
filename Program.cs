4b884f609de7540ce8f41141f81f0a6fabacd63b
900M100CK <chinhkhang105e@gmail.com>
2026-04-10 13:36:12 +0700

    allow all

--- Program.cs
+++ Program.cs
@@ -26,8 +26,16 @@ builder.Services.AddScoped<IShortenUrlService, ShortenUrlService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
 
 var app = builder.Build();
+
 
 if (app.Environment.IsDevelopment())
 {
@@ -40,6 +48,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 
+app.UseCors("AllowAll");
 app.MapControllers();
 
 app.Run();
